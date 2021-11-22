6ce6b115fd2a90ba4559046f33833a6f41bda0ad
Legend <salmurzaev1st@gmail.com>
2021-11-22 13:45:04 +0300

    admin page

---
+++ client/src/components/AdminJobsList/AdminJobsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './style.module.css'
+const AdminJobsList = () => {
+  return (
+    <div className={style.wrapper}>
+      Jobs
+    </div>
+  )
+}
+
+export default AdminJobsList
---
+++ client/src/components/AdminSerialsList/AdminSerialsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './style.module.css'
+const AdminSerialsList = () => {
+  return (
+    <div className={style.wrapper}>
+      Serials
+    </div>
+  )
+}
+
+export default AdminSerialsList
